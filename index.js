09b51c2a978669e1ff767e4fe191122cada5d856
Sofie <sokj0001@kea.stud.dk>
2024-09-16 20:41:32 +0200

    gæt et tal

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-//console.log("Math.random", Math.floor(Math.random()*(3+1)));//
+//laver tallene fra 1- 100// 
+let randomNumber = Math.floor(Math.random() * 101);
 
-//når man siger * 3 får man kun tallene 0,1,2//
-//når man siger *(3+1) får man også tallet 3 //
+//henter html elementer//
+const guessButton = document.querySelector('.guessButton');
+const guessInput = document.querySelector('.guessInput');
+const svar = document.querySelector('.svar');
 
-// console.log(Randomnumber);
+//tilføjer event listener til knappen//
+guessButton.addEventListener('click', function() {
+    let guess = Number(guessInput.value);
 
-function generateRandomNumber() {
-    let Randomnumber = Math.floor(Math.random()*(100+1));
+    if (guessInput.value === '' || guess < 0 || guess > 100) {
+        svar.textContent = 'Gæt et tal mellem 0 og 100';
+    } else if (guess === randomNumber) {
+        svar.textContent = "Jubiiii, det er rigtigt!!!!";
+    } else if (guess < randomNumber) {
+        svar.textContent = "For lavt! Prøv igen";
+    } else {
+        svar.textContent = "For højt! Prøv igen";
+    }
 
-    document.querySelector("h1").textContent= Randomnumber;
-}
-
-generateRandomNumber();
+    guessInput.value = '';
+    guessInput.focus();
+});
